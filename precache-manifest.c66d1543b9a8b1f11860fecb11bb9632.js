62c64f4d2e82e25d5ae8ca3750a45f393e46b065
Anish Vaidya <anishamulvaidya@gmail.com>
2020-08-10 14:22:32 -0700

    Updates

---
+++ precache-manifest.c66d1543b9a8b1f11860fecb11bb9632.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b3c2fc88070fa12f121e9cde3d2dddd0",
+    "url": "/test_deployment/index.html"
+  },
+  {
+    "revision": "e3724b569a95e491d2e6",
+    "url": "/test_deployment/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "e8542ef0dfd8e183eba8",
+    "url": "/test_deployment/static/js/2.03fe1b54.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/test_deployment/static/js/2.03fe1b54.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e3724b569a95e491d2e6",
+    "url": "/test_deployment/static/js/main.cda04078.chunk.js"
+  },
+  {
+    "revision": "b50cfc156ad223a96b38",
+    "url": "/test_deployment/static/js/runtime-main.5d0def2c.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/test_deployment/static/media/logo.5d5d9eef.svg"
+  }
+]);
